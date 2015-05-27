9eaedc77be801450bef89d296bb6e8da47f5942b
Emma Shakespeare <emma@wikblad.com>
2015-05-27 10:55:53 +0200

    Lite kommentarer inlagda samt kontroller klasser skapade

--- src/project/AdminUI.java
+++ src/project/AdminUI.java
@@ -1,5 +1,9 @@
 package project;
 
 public class AdminUI {
-
+/**
+ * Här startas admin applikationen 
+ * med drop down menyer, knappar och textfält där information 
+ * som hämats från databasen visas.
+ */
 }
--- src/project/UserUI.java
+++ src/project/UserUI.java
@@ -1,5 +1,9 @@
 package project;
 
 public class UserUI {
-
+/**
+ * Här startas User appen där användaren kan söka efter information i databasen.
+ * Det finns diverse dropdown menyer och knappar för önskad sökning samt ett textfält 
+ * som visar resultatet hämtat från databasen.
+ */
 }
